8ea3ad8c5685f5182ef6bd5ef574169f62992ea5
Сурен Давтян <surendavtan@DavtyanS.local>
2023-04-25 09:00:43 +0300

    Выполнили дз для семинара 9

---
+++ task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+void Even (int first, int second)
+{
+    if (first > second)
+    {
+        return;
+    }
+
+    if (first%2 == 0)
+    {
+    System.Console.WriteLine(first); 
+    }
+
+    Even(first+1, second);
+
+}
+
+Even (4, 8);
---
+++ task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 3: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 3, n = 2 -> A(m,n) = 29
+int Ack (int first, int second)
+{
+  if (first == 0)
+    return second + 1;
+  else
+    if ((first != 0) && (second == 0))
+      return Ack(first - 1, 1);
+    else
+      return Ack(first - 1, Ack(first, second - 1));
+}
+
+System.Console.WriteLine(Ack(3,2));
